15678b3a50b9962d8aee9558099b55e762ff71ef
aditG555 <152684971+aditG555@users.noreply.github.com>
2025-04-19 11:10:51 +0700

    Fixed the boiler
    The boiler now almost finished

--- Assets/Scripts/CraftingStuff/BoilerPan.cs
+++ Assets/Scripts/CraftingStuff/BoilerPan.cs
@@ -3,25 +3,61 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class BoilerPan : MonoBehaviour, IPointerDownHandler, IDropHandler
 {
     public GameObject ItemsGroup;
-    public List<GameObject> Recepy;
+    public List<ItemType> Recepy;
+    public GameObject OutPut;
 
     [SerializeField] UnityEvent Activate;
+    [SerializeField] UnityEvent End;
 
     [Header("Item Ouput List")]
     [Space(10)]
     [SerializeField] GameObject BroWhatOut;
     [SerializeField] GameObject JaheOut;
-    [SerializeField] GameObject KencurOut;
+    [SerializeField] GameObject TehAdasOut;
     [SerializeField] GameObject TemulawakOut;
     [SerializeField] GameObject KunyitOut;
     [SerializeField] GameObject SeraiOut;
-
+    [Space(10)]
+    //[Header("Item Input List")]
+    //[Space(10)]
+    //[SerializeField] GameObject Water;
+    //[SerializeField] GameObject Jahe;
+    //[SerializeField] GameObject Kencur;
+    //[SerializeField] GameObject Temulawak;
+    //[SerializeField] GameObject Kunyit;
+    //[SerializeField] GameObject Serai;
+    //[SerializeField] GameObject BijiAdas;
+    //[SerializeField] GameObject GulaPasir;
+    Image RebusImg;
+    Color originalColor;
+    private void Awake()
+    {
+        RebusImg = GetComponent<Image>();
+        End.AddListener(ClearRecepy);
+        originalColor = RebusImg.color;
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
+        if(OutPut != null)
+        {
+            GameObject OUtput = GameObject.Instantiate(OutPut, GameObject.Find("HandItem").transform);
+            if (GameManager._instance.AddItemInHand(OUtput))
+            {
+                End.Invoke();
+            }
+            else
+            {
+                Debug.Log("HandIsFull");
+                End.Invoke();
+                Destroy(OUtput);
+            }
+        }
     }
     public void OnDrop(PointerEventData eventData)
     {
@@ -29,29 +65,47 @@ public class BoilerPan : MonoBehaviour, IPointerDownHandler, IDropHandler
         {
             if (eventData.pointerDrag.GetComponent<DragAndDrop>().canbeBoil)
             {
-                DragAndDrop item = eventData.pointerDrag.GetComponent<DragAndDrop>();
-                //if(eventData.pointerDrag.GetComponent<DragAndDrop>().itemType == ItemType.)
-                item.ParentAfterDrag = ItemsGroup.transform;
-                item.canbeDrag = false;
-                Recepy.Add(item.gameObject);
-                switch (eventData.pointerDrag.GetComponent<DragAndDrop>().itemType)
+                if(Recepy.Count <= 11)
                 {
-                    case ItemType.Air:
-                        break;
-                    case ItemType.Jahe:
-                        break;
-                    default:
+                    DragAndDrop item = eventData.pointerDrag.GetComponent<DragAndDrop>();
+                    //if(eventData.pointerDrag.GetComponent<DragAndDrop>().itemType == ItemType.)
+                    item.ParentAfterDrag = ItemsGroup.transform;
+                    item.canbeDrag = false;
+                    Recepy.Add(item.itemType);
+                    if (Recepy.Contains(ItemType.Air))
+                    {
+                        Debug.Log("HasWater!");
+                        if (Recepy.Contains(ItemType.BijiAdas))
+                        {
+                            Debug.Log("HasBijiAdas!");
+                            if (Recepy.Contains(ItemType.GulaPasir))
+                            {
+                                Debug.Log("HasGulaPasir!");
+                                OutPut = TehAdasOut;
+                                RebusImg.color = Color.red;
 
-                        break;
+                            }
+                        }
+                    }
+                    if (!GameManager._instance.isFilled())
+                    {
+                        eventData.pointerDrag.GetComponent<DragAndDrop>().ShelveFrom.DencrementCount();
+                        Destroy(eventData.pointerDrag);
+                        Activate.Invoke();
+                    }
                 }
-                if (!GameManager._instance.isFilled())
-                {
-                    eventData.pointerDrag.GetComponent<DragAndDrop>().ShelveFrom.DencrementCount();
-                    Destroy(eventData.pointerDrag);
-                    Activate.Invoke();
-                }
+                
             }
 
         }
+    }
+    public void ClearRecepy()
+    {
+        Recepy.Clear();
+        foreach (Transform child in ItemsGroup.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        RebusImg.color = originalColor;
     }
 }
