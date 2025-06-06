bf6bc2dbc37e07c75e7daffa304a6844b91fa04b
aditG555 <152684971+aditG555@users.noreply.github.com>
2025-06-06 23:27:03 +0700

    LastPhase1

---
+++ Assets/DataDump.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DataDump : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameManager gameManager;
+    void Start()
+    {
+        gameManager = GameManager._instance;
+        foreach(ShelveItme shelve in gameManager.Shelve)
+        {
+            shelve.AddCount(10);
+        }
+    }
+}
